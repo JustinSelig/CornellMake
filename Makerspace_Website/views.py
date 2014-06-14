d51214515df263f83dd79c1dfc55681eb1921e69
Justin Selig <jss459@cornell.edu>
2014-06-13 23:53:21 -0400

    website can now handle static files

--- Makerspace_Website/views.py
+++ Makerspace_Website/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 """Returns an HttpResponse with string as Template"""
-def hello(request):
+def home(request):
 	return render(request, 'index.html')
