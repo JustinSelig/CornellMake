19b59b95078ec33356622749d110db9659e0ff48
Justin Selig <jss459@cornell.edu>
2014-06-24 21:20:56 -0400

    fields added to projects

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+	user = models.CharField(max_length=100) #associate user(s) with project?
+	name = models.CharField(max_length=100)
+	organization = models.CharField(max_length=100)
+	description = models.TextField(max_length=500)
+	email = models.EmailField(max_length=100)
+	image = models.CharField(max_length=200, default="http://www.placekitten.com/200/300/", null=True)
