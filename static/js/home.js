bb293b7d2a2b59a3834b15a2c4d32b2cab70c664
Justin Selig <jss459@cornell.edu>
2014-07-24 23:11:17 -0400

    fixed project creation form

--- static/js/home.js
+++ static/js/home.js
@@ -1,4 +1,3 @@
-
 var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
 var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
 var images = document.getElementsByClassName('bg-fader-img');
