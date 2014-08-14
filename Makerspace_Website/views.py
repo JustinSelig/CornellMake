e349831344e28bb5d108550ff472a6e462b38631
Justin Selig <jss459@cornell.edu>
2014-08-14 16:13:05 -0400

    admin approve works, very simple search works

--- Makerspace_Website/views.py
+++ Makerspace_Website/views.py
@@ -2,6 +2,7 @@ from django.template.loader import get_template
 from django.template import Context
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
+from projects.models import Project
 
 #def home(request):
 #	return render(request, 'index.html')
@@ -13,4 +14,8 @@ def media(request):
 	return render(request, 'media.html')
 
 def connect(request):
+	if 'query' in request.GET:
+		query = request.GET['query']
+		projects = Project.objects.filter(name__icontains=query)
+		return render(request, 'connect.html', {'projects':projects, 'query':query})
 	return render(request, 'connect.html')
--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,7 @@ from forms import ProjectSubmissionForm
 from django.core.context_processors import csrf
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from models import ProjectSubmission
+from models import ProjectSubmission, Project
 
 def create(request):
 	if request.method == 'POST':
@@ -18,4 +18,36 @@ def create(request):
 	return render(request, 'create.html', args)
 
 def approve(request):
+	if request.method == 'POST':
+		approved_submissions = request.POST.getlist('approved')
+		disapproved_submissions = request.POST.getlist('disapproved')
+		for submission in approved_submissions: #submission is unicode id
+			print "submission: " + `submission`
+			old = ProjectSubmission.objects.get(id=submission)
+#			#submission description update by admin
+#			name = 'description-' + str(submission)
+#			obj.description = request.POST.get(name)
+#			obj.save()
+			new = Project.objects.create()
+#			new.user_id = obj.user_id
+			new.name = old.name
+			new.email = old.email
+			new.organization = old.organization
+			new.website = old.website
+			new.idea_name = old.idea_name
+			new.description = old.description
+			new.category = old.category
+			new.image = old.image
+#			increase = new.approve()
+			new.save()
+			old.delete()
+		for submission in disapproved_submissions:
+			old = ProjectSubmission.objects.get(id=submission)
+#			#user =  User.objects.get(id=obj.user_id)
+#			obj.disapprove()
+			old.delete()
+		args = {}
+		args.update(csrf(request))
+		args['submissions'] = ProjectSubmission.objects.all()
+		return render(request, 'admin_approve.html', args)
 	return render(request, 'admin_approve.html', {'submissions':ProjectSubmission.objects.all()})
